a99088035cce4cee9423ba3ed8684f6384ee30c7
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-29 22:59:08 +0100

    Refactor package.json to update express version and add express-async-errors dependency

--- Server/app.js
+++ Server/app.js
@@ -1,1 +1,35 @@
-console.log('Server is running');
+import 'dotenv/config';
+import 'express-async-errors';
+import express from 'express';
+import { notFound } from './middleware/not-found.js';
+import { errorHandlerMiddleware } from './middleware/error-handler.js';
+import { connectDB } from './db/connect.js';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API');
+});
+
+// Catch All
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+// Connect to DB and start the server
+const start = async () => {
+  try {
+    // await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
